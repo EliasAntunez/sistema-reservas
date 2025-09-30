bf900166f76899c1b932dba8ac4da04bc90bc4bc
Elias E. Antunez <eliasantunez172@gmail.com>
2025-09-30 19:38:25 -0300

    Inicializando el proyecto.

---
+++ tureserva/src/main/java/com/example/tureserva/TureservaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tureserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TureservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TureservaApplication.class, args);
+	}
+
+}
